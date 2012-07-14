e4c2309892352a389d88455a7ddbb5f7d7906bee
Marc SCHNEIDER <marc@cinematiko>
2012-07-14 18:23:14 +0200

    First version

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -1,42 +1,74 @@
-int dirpin = 3;
-int enablepin = 4;
-int steppin = 12;
+/*
+ Maximal speed : 3 microseconds delay
+ 5000 microseconds is a very low speed
+*/
+
+// Defines for setting and clearing register bits
+// This is to speed up analog read
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+
+int DIR_PIN = 3;
+int ENABLE_PIN = 4;
+int STEP_PIN = 2;
+
+int MOTOR_CALIBRATE = 1750;
+int MOTOR_MIN_DELAY = 3;
+int MOTOR_MAX_DELAY = 5000;
+
+int DEAD_BAND_INF = 505;
+int DEAD_BAND_MID = 511;
+int DEAD_BAND_SUP = 515;
+
+int sensorCurValue = 0;
+int motorDelay = 0;
+
 
 void setup() {
-    Serial.begin(9600);
-	pinMode(dirpin, OUTPUT);
-	pinMode(enablepin, OUTPUT);
-	pinMode(steppin, OUTPUT);
-
-	digitalWrite(enablepin, HIGH);
+  // To speed up analog read, set prescale to 16
+  sbi(ADCSRA,ADPS2) ;
+  cbi(ADCSRA,ADPS1) ;
+  cbi(ADCSRA,ADPS0) ;
+  
+  Serial.begin(9600);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  
+  digitalWrite(ENABLE_PIN, HIGH);
 }
 
 void loop() {
-    int motorDelay;
-    int sensorValue = analogRead(A0);
-    Serial.println(sensorValue, DEC);
-    int relV = sensorValue / 2;
-    if (sensorValue <= 611) {
-      digitalWrite(dirpin, LOW); // Set the direction
-      motorDelay = 611 - (relV * 3 / 611);
-    }
-    else {
-      digitalWrite(dirpin, HIGH); // Set the direction
-      motorDelay = relV * 3 / 611;
-    }
-    
+  sensorCurValue = analogRead(A1); // Get a value from 0 to 1023
+  int relativeV = sensorCurValue - DEAD_BAND_MID;
+  if (sensorCurValue <= DEAD_BAND_INF) { // Define a "dead band" so that the robot doesn't move within a range of values
+    digitalWrite(DIR_PIN, LOW); // Set the direction
+    motorDelay = map(sensorCurValue, 0, DEAD_BAND_INF, MOTOR_MIN_DELAY, MOTOR_MAX_DELAY);
     move(motorDelay);
-  
-	//stop();
+  }
+  else if (sensorCurValue >= DEAD_BAND_SUP) {
+    digitalWrite(DIR_PIN, HIGH); // Set the direction
+    motorDelay = map(sensorCurValue, DEAD_BAND_SUP, 1023, MOTOR_MAX_DELAY, MOTOR_MIN_DELAY);
+    move(motorDelay);
+  }
+  else {
+    digitalWrite(ENABLE_PIN, HIGH);
+  }
+  //stop();
 }
 
 void stop() {
-    digitalWrite(enablepin, LOW);
+  digitalWrite(ENABLE_PIN, LOW);
 }
 
 void move(int microSecs) {
-	digitalWrite(steppin, LOW);
-	delayMicroseconds(microSecs);
-	digitalWrite(steppin, HIGH);
-	//delayMicroseconds(microSecs);
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds(microSecs);
 }

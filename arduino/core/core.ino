5c67251998d744adf469a6157a71298c7c842f6b
Marc SCHNEIDER <marc@cinematiko>
2012-08-20 13:20:18 +0200

    Write / read with sd card and use time / nbImpuls for records

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -1,7 +1,9 @@
 /*
  Control a brushless motor using a potentiometer
  Authors : Alexandre Massot and Marc Schneider
-*/
+ */
+
+#include <SD.h>
 
 // Defines for setting and clearing register bits
 // This is to speed up analog read
@@ -12,12 +14,15 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-// Don't use pin 4 for the moment as it is used by SD card
+// Pins for the motor
 const byte DIR_PIN = 3;
 const byte ENABLE_PIN = 5;
 const byte STEP_PIN = 2;
 
 const int SENSOR_PIN_1 = A1;
+
+const byte SD_CARD_PIN = 4;
+const byte SD_CARD_SS_PIN = 10;
 
 const unsigned int MOTOR_MIN_DELAY = 3;
 const unsigned int MOTOR_MAX_DELAY = 5000;
@@ -44,12 +49,15 @@ const char GOTO_START = 'g'; // Go to start point of last record
 
 long curNbImpuls = 0; // Current motor position
 long startRecordX = 0; // Current motor position when start recording
-const unsigned int MAX_RECORDS = 3500;
+const unsigned int MAX_RECORDS = 100000;
+File recordFile; // Used to store records
+char * recordFileName = "records.txt";
+
 int nbRecords = 0;
 const unsigned int RECORD_FREQ = 10; // in ms
 unsigned long lastReadTime = 0; // when the last reading from input device was done
 unsigned long lastRecordTime = 0;
-unsigned int recordValues[MAX_RECORDS];
+
 
 // Constants used when computing mapping between input value (sensor) and output value (motor)
 const float MAP_HIGH = (float)abs(MOTOR_MAX_DELAY - MOTOR_MIN_DELAY) / (float)abs(1023 - DEAD_BAND_SUP);
@@ -62,14 +70,18 @@ void setup() {
   sbi(ADCSRA,ADPS2) ;
   cbi(ADCSRA,ADPS1) ;
   cbi(ADCSRA,ADPS0) ;
-  
+
   Serial.begin(9600);
+
+  // Motor initialization
   pinMode(DIR_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
-  
-  digitalWrite(ENABLE_PIN, HIGH);
-  
+  digitalWrite(ENABLE_PIN, HIGH); // Enable motor
+
+  // SD card initialization
+  if (! initSdCard()) return;
+
   nbRecords = 0;
 }
 
@@ -85,6 +97,7 @@ void mainLoop() {
       startRecordX = curNbImpuls;
       nbRecords = 0;
       Serial.println("Recording movement");
+      if (! initRecordFile()) return;
     }
   }
   if (cmdRead == MOVE) {
@@ -93,13 +106,7 @@ void mainLoop() {
   }
   else if (cmdRead == RECORD) {
     if (nbRecords < MAX_RECORDS) {
-      sensorCurValue = readValueFromSensor(SENSOR_PIN_1);
-      if (millis() - lastRecordTime >= RECORD_FREQ) {
-        recordValues[nbRecords] = sensorCurValue;
-        lastRecordTime = millis();
-        nbRecords++;
-      }
-      move(sensorCurValue);
+      recordMovement();
     }
     else {
       Serial.println("Stop : max nb of records reached!");
@@ -111,21 +118,7 @@ void mainLoop() {
     cmdRead = NONE;
   }
   else if (cmdRead == PLAY) {
-    moveToStartRecord();
-    Serial.println("Mvt replay");
-    long nbImpuls = 0;
-    for (int i=0; i<nbRecords; i++) {
-      unsigned long curTime = millis();
-      // Replay sampling
-      while (millis() - curTime < RECORD_FREQ) {
-        move(recordValues[i]);
-        readValueFromSensor(SENSOR_PIN_1); // Just to simulate the delay introduced when recording values
-        nbImpuls++;
-      }
-    }
-    digitalWrite(ENABLE_PIN, HIGH);
-    Serial.println("Ok");
-    lastRecordTime = 0;
+    replayMovement();
     cmdRead = STOP;
   }
   else if (cmdRead == ZERO) {
@@ -138,6 +131,7 @@ void mainLoop() {
   }
   else if (cmdRead == STOP) {
     digitalWrite(ENABLE_PIN, HIGH);
+    recordFile.close();
     cmdRead = NONE;
     Serial.println("Stop");
     displayCurInfos();
@@ -159,7 +153,7 @@ void move(int sensorCurValue) {
     lastDirLow = true;
     motorDelay = mapAnalogToDelayLow(sensorCurValue, 0, DEAD_BAND_INF, MOTOR_MIN_DELAY, MOTOR_MAX_DELAY);
     //motorDelay = map(sensorCurValue, 0, DEAD_BAND_INF, MOTOR_MIN_DELAY, MOTOR_MAX_DELAY);
-    moveOneStep(motorDelay, false);
+    moveOneImpuls(motorDelay, false);
   }
   else if (isMovingForward(sensorCurValue)) {
     if (lastDirLow) {
@@ -168,17 +162,18 @@ void move(int sensorCurValue) {
     lastDirLow = false;
     motorDelay = mapAnalogToDelayHigh(sensorCurValue, DEAD_BAND_SUP, 1023, MOTOR_MIN_DELAY, MOTOR_MAX_DELAY);
     //motorDelay = map(sensorCurValue, DEAD_BAND_SUP, 1023, MOTOR_MAX_DELAY, MOTOR_MIN_DELAY);
-    moveOneStep(motorDelay, true);
+    moveOneImpuls(motorDelay, true);
   }
   else {
     digitalWrite(ENABLE_PIN, HIGH);
   }
 }
 
-void moveOneStep(int microSecs, boolean isForward) {
+void moveOneImpuls(int microSecs, boolean isForward) {
   digitalWrite(STEP_PIN, LOW);
   digitalWrite(STEP_PIN, HIGH);
-  if (isForward) curNbImpuls++; else curNbImpuls--;
+  if (isForward) curNbImpuls++; 
+  else curNbImpuls--;
   delayMicroseconds(microSecs);
 }
 
@@ -193,7 +188,7 @@ void moveRelative(long nbImpuls) {
   }
   nbImpuls = abs(nbImpuls);
   for (long i=0; i<nbImpuls; i++) {
-    moveOneStep(CRUISE_MOTOR_DELAY, (nbImpuls > 0));
+    moveOneImpuls(CRUISE_MOTOR_DELAY, (nbImpuls > 0));
   }
   Serial.println("Ok");
 }
@@ -209,6 +204,60 @@ void moveToStartRecord() {
   //Serial.println(curNbImpuls);
   //Serial.println(startRecordX);
   moveRelative(curNbImpuls - startRecordX);
+}
+
+void recordMovement() {
+  sensorCurValue = readValueFromSensor(SENSOR_PIN_1);
+  move(sensorCurValue);
+  long curRecordTime = millis();
+  //recordFile.println("Hello");
+  // Do sampling
+  if (curRecordTime - lastRecordTime >= RECORD_FREQ) {
+    recordFile.println(String(curRecordTime) + ";" + String(curNbImpuls));
+    //recordValues[nbRecords] = sensorCurValue;
+    lastRecordTime = millis();
+    nbRecords++;
+  }
+}
+
+void replayMovement() {
+  moveToStartRecord();
+  Serial.println("Mvt replay");
+  recordFile = SD.open(recordFileName);
+  if (recordFile) {
+    Serial.println("Reading from file : " + String(recordFileName));
+    long curRecordTimeRead=0, curNbImpulsRead=0, prevRecordTimeRead=0, prevNbImpulsRead=0;
+    if (recordFile.available()) {
+      prevRecordTimeRead = recordFile.parseInt();
+      prevNbImpulsRead = recordFile.parseInt();
+    }
+    float delayMsBetween2Impuls;
+    while (recordFile.available()) {
+      curRecordTimeRead = recordFile.parseInt();
+      curNbImpulsRead = recordFile.parseInt();
+      if (curRecordTimeRead - prevRecordTimeRead > 0) {
+        //Serial.println(curRecordTimeRead - prevRecordTimeRead);
+        //Serial.println(curNbImpulsRead - prevNbImpulsRead);
+        delayMsBetween2Impuls = 1/abs((float)(curNbImpulsRead - prevNbImpulsRead) / (float)(curRecordTimeRead - prevRecordTimeRead));
+        Serial.println(delayMsBetween2Impuls);
+        unsigned long curTime = millis();
+        // Replay sampling
+        while (millis() - curTime < RECORD_FREQ) {
+          move(delayMsBetween2Impuls * 1000); // to get it in micro secs
+          readValueFromSensor(SENSOR_PIN_1);  // Just to simulate the delay introduced when recording values
+        }
+        prevRecordTimeRead = curRecordTimeRead;
+        prevNbImpulsRead = curNbImpulsRead;
+      }
+    }
+    digitalWrite(ENABLE_PIN, HIGH);
+    Serial.println("Ok");
+    lastRecordTime = 0;
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("Error opening file");
+  }  
 }
 
 void stop() {
@@ -239,7 +288,42 @@ float mapAnalogToDelayHigh(float x, float in_min, float in_max, float out_min, f
 float mapAnalogToDelayLow(float x, float in_min, float in_max, float out_min, float out_max) {
   //return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   return (x - in_min) * MAP_LOW + (float)MOTOR_MIN_DELAY;
-} 
+}
+
+boolean initSdCard() {
+  Serial.print("Initializing SD card...");
+  // For Mega boards with an Ethernet shield, make sure the Wiznet
+  // chip is not selected:
+  pinMode(SD_CARD_SS_PIN, OUTPUT);
+  digitalWrite(SD_CARD_SS_PIN, HIGH); // davekw7x: If it's low, the Wiznet chip corrupts the SPI bus
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_CARD_PIN)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return false;
+  }
+  Serial.println("card initialized.");
+  return true;
+}
+
+boolean initRecordFile() {
+  if (SD.exists(recordFileName)) {
+    if (SD.remove(recordFileName)) {
+      Serial.println("Existing file removed : " + String(recordFileName));
+    }
+    else {
+      Serial.println("Fail to remove existing file : " + String(recordFileName));
+    }
+  }
+
+  // Open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  recordFile = SD.open(recordFileName, FILE_WRITE);
+  if (! recordFile) return false;
+  Serial.println("Created file to record values : " + String(recordFileName));
+  return true;
+}
 
 void displayCurInfos() {
   Serial.println("Cur pos:" + String(curNbImpuls));
@@ -252,3 +336,5 @@ int freeRam () {
   int v; 
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
+
+
